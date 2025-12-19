8f4ab238ff659339ebd377be11a260da8f44a5f3
Jack Frederick <jfred@Jacks-MacBook-Air-175.local>
2025-12-19 13:31:43 -0500

    Fix missing useEffect import

--- portfolio-site/src/components/ContentTemplate.jsx
+++ portfolio-site/src/components/ContentTemplate.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 
 export default function ContentTemplate({ title, textSections, images }) {
   const [index, setIndex] = useState(0);
